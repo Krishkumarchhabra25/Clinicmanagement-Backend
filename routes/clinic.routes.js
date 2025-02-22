ffe35074436d63474498212a531e97a0f333592c
krishkumarchhabra <123287035+KrishKuChhabra@users.noreply.github.com>
2025-02-23 02:44:53 +0530

    omeything chnages

--- routes/clinic.routes.js
+++ routes/clinic.routes.js
@@ -2,20 +2,21 @@ const express = require('express');
 const router = express.Router();
 const { upload } = require('../middlewares/multer');
 const clinicController = require("../controller/clinic.user.controller");
+const { checkRole, adminAuthUser } = require('../middlewares/auth.middleware');
 
 // Image Upload Route
-router.post('/upload-image', upload, clinicController.handleImageUpload);
+router.post('/upload-image', upload, clinicController.handleImageUpload , checkRole(['admin']) ,adminAuthUser );
 
 // Basic Info Routes
-router.post('/add-basic-info', upload, clinicController.addBasicInfo);
-router.put('/update-basic-info', upload, clinicController.updateBasicInfo);
+router.post('/add-basic-info', upload, clinicController.addBasicInfo , checkRole(['admin']) , adminAuthUser) ;
+router.put('/update-basic-info', upload, clinicController.updateBasicInfo , checkRole(['admin']) , adminAuthUser);
 
 // Address Routes
-router.post('/add-address', clinicController.addAddress);
-router.put('/update-address', clinicController.updateAddress);
+router.post('/add-address', clinicController.addAddress , checkRole(['admin']) , adminAuthUser);
+router.put('/update-address', clinicController.updateAddress ,checkRole(['admin']) , adminAuthUser);
 
 // Get Clinic Profile
-router.get('/clinic', clinicController.getClinicProfile);
+router.get('/clinic', clinicController.getClinicProfile ,    checkRole(['admin']), adminAuthUser);
   
   
 
